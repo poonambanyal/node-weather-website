1e305e0539abdd7f0d47cb454731c84f535311db
poonambanyal <poonambanyal88@gmail.com>
2020-10-12 23:05:28 +0530

    added humidity in weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast=(latitude,longitude ,callback)=>{
             callback('unable to find location',undefined)
         }
         else{
-            callback(undefined,'It is currently ' +body.current.temperature +' degrees out.It feels like '+body.current.feelslike+' degrees out')
+            callback(undefined,'It is currently ' +body.current.temperature +' degrees out.It feels like '+body.current.feelslike+' degrees out. And Humidity is '+ body.current.humidity)
         
         }
         })
